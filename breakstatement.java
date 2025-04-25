28d3b4234f66f5efaef6ba48bef2c94837fc6cd3
Nishant Raghav <159788393+nishantraghav01@users.noreply.github.com>
2025-04-25 23:09:52 +0530

    Add files via upload

---
+++ breakstatement.java
@@ -0,0 +1,14 @@
+public class breakstatement{
+    public static void main(String[] args){
+        int i =1;
+        for(i=1;i<=5;i++){
+            if(i==3){
+            break;
+        }
+        System.out.println(i);
+         }  
+        System.out.println("I am out of the loop");
+
+    
+    }
+}
---
+++ dowhileloop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class dowhileloop{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int counter=1;
+        do {
+            System.out.println(counter);
+            counter++;
+        }
+        while(counter<=10);
+    }
+ {
+    }
+}
